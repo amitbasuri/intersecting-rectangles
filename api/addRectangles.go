3eed528332ea6bd0a193864ded869198047a0cd0
Amit Basuri <amitbasuri123@gmail.com>
2020-05-06 02:22:44 +0530

    added some checks for bad input

--- api/addRectangles.go
+++ api/addRectangles.go
@@ -6,7 +6,12 @@ type AddRectanglesRequest struct {
 
 func (p *TwoDimensionalPlane) AddRectangles(addRectRequest AddRectanglesRequest) {
 	count := 1
+	empty := Rectangle{}
 	for _, r := range addRectRequest.Rectangles {
+		if r == empty || r.W <= 0 || r.H <= 0 {
+			continue
+		}
+
 		p.Rectangles[count] = r
 		count++
 	}
--- main.go
+++ main.go
@@ -17,7 +17,6 @@ func main() {
 	}
 	flag.Parse()
 
-	fmt.Println(inputFilePath)
 	inputFile, err := os.Open(*inputFilePath)
 	if err != nil {
 		log.Fatal("unable to open input file: ", inputFilePath)
@@ -31,12 +30,15 @@ func main() {
 		log.Fatal("unable to decode input file: ", err.Error())
 	}
 
+	// create 2D space
 	twoDimPlane := api.NewTwoDimPlane()
 	if len(request.Rectangles) > 10 {
 		request.Rectangles = request.Rectangles[:10]
 	}
 
+	// add rectangles to the 2D space
 	twoDimPlane.AddRectangles(request)
+
 	n := len(twoDimPlane.Rectangles)
 	fmt.Println("\nInput:")
 	for k := 1; k <= n; k++ {
@@ -44,16 +46,19 @@ func main() {
 		fmt.Printf("     %d: Rectangle at (%d, %0.d), w=%d ,h=%d.\n", k, int(v.X), int(v.Y), v.W, v.H)
 	}
 
+	// calculate intersections
 	twoDimPlane.AddDoubleIntersections()
-	i := 1
+	twoDimPlane.AddTripleIntersections()
+
+	// print intersections
 	fmt.Println("Intersections:")
 
+	i := 1
 	for k, v := range twoDimPlane.DoubleIntersectedRectangle {
 		arr := strings.Split(k, "_")
 		fmt.Printf("     %d: Between rectangle %s and %s at (%d,%d), w=%d, h=%d.\n", i, arr[0], arr[1], int(v.X), int(v.Y), v.W, v.H)
 		i++
 	}
-	twoDimPlane.AddTripleIntersections()
 
 	for k, v := range twoDimPlane.TripleIntersectedRectangle {
 		arr := strings.Split(k, "_")
